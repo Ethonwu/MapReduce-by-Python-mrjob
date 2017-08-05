e423f0a5d0784dd03b0c8bc539561393bd73173c
a5351998 <a5351998@gmail.com>
2017-08-05 23:22:30 +0800

    Update wordcount

--- wordcount.py
+++ wordcount.py
@@ -1,8 +1,7 @@
 from mrjob.job import MRJob
 class MRWordCount(MRJob):
     def mapper(self, _, line):
-        for letter in line:
-            for word in letter:
+        for word in line.split():
                 yield(word, 1)
     def reducer(self, word, counts):
             yield(word, sum(counts))
