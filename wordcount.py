89089df926ba0efe791afb3f7e8a39407c50955e
a5351998 <a5351998@gmail.com>
2017-08-12 20:00:18 +0800

    Example

--- wordcount.py
+++ wordcount.py
@@ -4,6 +4,7 @@ class MRWordCount(MRJob):
         for word in line.split():
                 yield(word, 1)
     def reducer(self, word, counts):
-            yield(word, sum(counts))
+        yield(word, sum(counts))
 if __name__ == '__main__':
     MRWordCount.run()
+    
